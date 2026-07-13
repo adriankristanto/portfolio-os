c1d2ed54aed6eb4b48006cd23fae5b1968b407a6
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2026-07-13 23:12:27 +0700

    chore: add flat entrypoint for importing prettier config for eslint

--- eslint.config.js
+++ eslint.config.js
@@ -4,7 +4,7 @@ import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 import jsxA11y from "eslint-plugin-jsx-a11y";
-import prettierConfig from "eslint-config-prettier";
+import prettierConfig from "eslint-config-prettier/flat";
 import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
